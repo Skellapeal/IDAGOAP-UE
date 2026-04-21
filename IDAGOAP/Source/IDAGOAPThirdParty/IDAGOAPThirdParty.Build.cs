75e74e07ecd9858ecbcd97a58fdb919a7a0b46e2
Niall Ó Colmáin <ocolmain.niall.92@protonmail.com>
2026-04-21 21:08:11 +0100

    Updated outdated build file

--- IDAGOAP/Source/IDAGOAPThirdParty/IDAGOAPThirdParty.Build.cs
+++ IDAGOAP/Source/IDAGOAPThirdParty/IDAGOAPThirdParty.Build.cs
@@ -9,10 +9,8 @@ public class IDAGOAPThirdParty : ModuleRules
 
         string ThirdPartyPath = Path.Combine(ModuleDirectory, "..", "..", "ThirdParty", "IDAGOAPCore");
 
-        // Add public include path so dependent modules can #include "rida_goap/..."
         PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "Include"));
 
-        // Link the static lib on Win64
         if (Target.Platform == UnrealTargetPlatform.Win64)
         {
             PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "Lib", "Win64", "IDAGOAP.lib"));
